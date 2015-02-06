b656d8dabd08ffe1d70bc77575305f7969fad86e
Johan Paulsson <gonz@badcode.net>
2015-02-06 15:24:56 -0800

    Removed sensor from craftable and added electronic circuits to rail

--- prototypes/recipe/rail-sensor/recipes.lua
+++ prototypes/recipe/rail-sensor/recipes.lua
@@ -1,27 +1,17 @@
 data:extend(
 {
-  {
-    type = "recipe",
-    name = "smart-chest-rail-sensor",
-    enabled = "true",
-    ingredients =
-    {
-      {"steel-chest", 1},
-      {"electronic-circuit", 3}
-    },
-    result = "smart-chest-rail-sensor"
-  },
-  {
-    type = "recipe",
-    name = "rail-sensor",
-    enabled = "true",
-    ingredients =
-    {
-      {"stone", 1},
-      {"iron-stick", 1},
-      {"steel-plate", 1}
-    },
-    result = "rail-sensor",
-    result_count = 2
-  }
+	{
+		type = "recipe",
+		name = "rail-sensor",
+		enabled = "true",
+		ingredients =
+		{
+			{"stone", 1},
+			{"iron-stick", 1},
+			{"steel-plate", 1},
+			{"electronic-circuit", 3}
+		},
+		result = "rail-sensor",
+		result_count = 2
+	}
 })
