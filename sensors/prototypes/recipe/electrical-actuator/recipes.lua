1c6dfa4cae9ebc6e5fc6d4304891adb1e007025c
Johan Paulsson <gonz@badcode.net>
2015-02-10 13:59:02 -0800

    Changed recipe for electrical actuator

--- sensors/prototypes/recipe/electrical-actuator/recipes.lua
+++ sensors/prototypes/recipe/electrical-actuator/recipes.lua
@@ -1,14 +1,16 @@
 data:extend(
 {
 	{
-    type = "recipe",
-    name = "electrical-actuator",
-    energy_required = 10,
-    enabled = "true",
-    ingredients =
-    {
-      {"iron-plate", 2}
-    },
-    result = "electrical-actuator"
-  }
+		type = "recipe",
+		name = "electrical-actuator",
+		energy_required = 10,
+		enabled = "true",
+		ingredients =
+		{
+			{"copper-plate", 2},
+			{"steel-plate", 2},
+			{"electronic-circuit", 1}
+		},
+		result = "electrical-actuator"
+	}
 })
